feea79a47cd87b9e673bca09cca8fe5fc5139cdd
Matthew <matthewmics77@gmail.com>
2022-11-13 19:17:22 +0800

    Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './footer.scss'
+import { AiFillGithub } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>MATTHEW MICLAT</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://github.com/matthewmics"><AiFillGithub /></a>
+      </div>
+    </footer>
   )
 }
 
