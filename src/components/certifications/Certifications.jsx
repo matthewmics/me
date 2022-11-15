6bc3e8954d96d8c6cf82bdc60886d801b01b1083
Matthew <matthewmics77@gmail.com>
2022-11-15 19:43:53 +0800

    Certifications component

---
+++ src/components/certifications/Certifications.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './certifications.scss';
+
+const Certifications = () => {
+  return (
+    <section>
+        <h5>My Certifications</h5>
+        <h2>Certifications</h2>
+    </section>
+  )
+}
+
+export default Certifications
